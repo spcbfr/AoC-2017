e536d73be19460876f1e0fc1f52d04ec624f9f56
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-11-21 20:41:41 +0100

    Solve the second 1/2 of day 1 and refactor the answer of the first 1/2..

--- day1.py
+++ day1.py
@@ -1,13 +1,31 @@
 f = open("input.txt", "r")
 txt = f.read()
 
-count = 0
-for index, value in enumerate(txt):
-    if index != len(txt) - 1:
-        if txt[index + 1] == value:
-            count = count + int(value)
-        pass
+
+# This will get the proper item of a list
+# when given an index, if the given index
+# is out-of-range the function will loop
+# back and start from the first index
+def doura(index: int, list: list[int]):
+    length = len(list)  # 5
+    if index < length:
+        return list[index]
     else:
-        if txt[0] == value:
-            count = count + int(value)
+        return list[index % length]
+
+
+count = 0
+midway = int(len(txt) / 2)
+print(midway)
+
+# Refactored answer for the first half of the question
+# for index, value in enumerate(txt):
+#     if doura(index + 1, txt) == value:
+#         count = count + int(value)
+
+
+for index, value in enumerate(txt):
+    if doura(index + midway, txt) == value:
+        count = count + int(value)
+
 print(count)
